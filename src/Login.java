4c3b85986fd9e5c859e9b17cc10eda9ecf3b97ef
KushalPatel9900 <49111690+KushalPatel9900@users.noreply.github.com>
2019-11-21 16:28:32 +0530

    Project Added

--- src/Login.java
+++ src/Login.java
@@ -309,7 +309,7 @@ public class Login extends javax.swing.JFrame {
         {  
             Class.forName("com.mysql.cj.jdbc.Driver");  
             con=DriverManager.getConnection(  
-            "jdbc:mysql://localhost:3306/lib_javaf","root","1357924680");  
+            "jdbc:mysql://localhost:3306/lib_javaf","YOUR_USERNAME","PASSWORD");  
             stm=con.createStatement(); 
         }
         catch(Exception e)

757ce283c981967f4b8f1f93972db203e38f50ac
KushalPatel9900 <49111690+KushalPatel9900@users.noreply.github.com>
2019-11-21 16:25:47 +0530

    Project Added

--- src/Fine.java
+++ src/Fine.java
@@ -265,7 +265,7 @@ public class Fine extends javax.swing.JFrame {
         {  
             Class.forName("com.mysql.cj.jdbc.Driver");  
             con=DriverManager.getConnection(  
-            "jdbc:mysql://localhost:3306/lib_javaf","root","1357924680");  
+            "jdbc:mysql://localhost:3306/lib_javaf","YOUR_USERNAME","PASSWORD");  
             stmt=con.createStatement(); 
         }
         catch(Exception e)
